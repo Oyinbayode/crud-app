092dbeb595fe545c2a25410447bf94b2c260f802
Oyinbayode <oyinbayode75@gmail.com>
2022-02-18 13:27:56 +0100

    Mira Test 1

---
+++ src/components/AddTask/AddTask.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
+import { yupResolver } from "@hookform/resolvers";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    Title: yup.string().required(),
+    Body: yup.string().required(),
+  })
+  .required();
+
+const AddTask = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => console.log(watch(data));
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
+          <label>Title</label>
+          <input {...register("Title")} />
+          <p>{errors.Title?.message}</p>
+        </FormControl>
+        <FormControl>
+          <label>Body</label>
+          <input {...register("Body")} />
+          <p>{errors.Body?.message}</p>
+        </FormControl>
+        <InputButton
+          type="submit"
+          className="btn btn-block"
+          value="Save Task"
+        />
+      </Form>
+    </>
+  );
+};
+
+const Form = styled.form`
+  margin-bottom: 40px;
+`;
+
+const FormControl = styled.div`
+  margin: 20px 0;
+  input {
+    width: 100%;
+    height: 40px;
+    margin: 5px;
+    padding: 3px 7px;
+    font-size: 17px;
+  }
+  input:focus {
+    outline: 0;
+  }
+  label {
+    display: block;
+  }
+`;
+
+const InputButton = styled.input`
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+
+  &:focus {
+    outline: none;
+    color: white;
+  }
+`;
+
+export default AddTask;
