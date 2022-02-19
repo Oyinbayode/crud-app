dbfa730453cc229b7140ba4406dfd7c85e90eed9
Oyinbayode <oyinbayode75@gmail.com>
2022-02-19 22:21:25 +0100

    feat: integrated delete + edit functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import Swal from "sweetalert2";
 import AddList from "./components/AddList/AddList";
 import Header from "./components/Header/Header";
 import Lists from "./components/Lists/Lists";
+import { v4 as uuid } from "uuid";
+import axios from "axios";
 
 function App() {
   const [lists, setLists] = useState([]);
   const [showAddList, setShowAddList] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  // Pre-loader
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, []);
+
+  // Fetch Data from local storage
+  const getLists = JSON.parse(localStorage.getItem("listAdded"));
+  useEffect(() => {
+    if (getLists === null) {
+      setLists([]);
+    } else {
+      setLists(getLists);
+    }
+  }, [getLists]);
 
   // Add List
   const addList = (list) => {
-    const id = Math.random() * 1000;
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    const id = uuid();
     const newList = { id, ...list };
     setLists([...lists, newList]);
     Swal.fire({
@@ -18,22 +43,84 @@ function App() {
       title: "Successful",
       text: "You have successfully added a new List!",
     });
+    localStorage.setItem("listAdded", JSON.stringify([...lists, newList]));
+  };
+
+  // Delete List
+  const deleteList = (id) => {
+    const deleteList = lists.filter((task) => task.id !== id);
+    setLists(deleteList);
+    Swal.fire({
+      icon: "success",
+      title: "Awwwn...",
+      text: "You have successfully deleted the List!",
+    });
+    localStorage.setItem("listAdded", JSON.stringify(deleteList));
+  };
+
+  // Edit List
+  const editList = (id) => {
+    const Title = prompt("Title");
+    const Body = prompt("Body");
+    let data = JSON.parse(localStorage.getItem("listAdded"));
+    const myData = data.map((x) => {
+      if (x.id === id) {
+        return {
+          ...x,
+          Title,
+          Body,
+          id: uuid(),
+        };
+      }
+      return x;
+    });
+    Swal.fire({
+      icon: "success",
+      title: "Yay...",
+      text: "You have successfully edited an existing task!",
+    });
+    localStorage.setItem("listAdded", JSON.stringify(myData));
+    window.location.reload();
   };
 
   return (
     <>
-      <div className="container">
-        {/* App Header */}
-        <Header
-          changeTextAndColor={showAddList}
-          showForm={() => setShowAddList(!showAddList)}
-        />
-        {/* Revealing the Add List Form */}
-        {showAddList && <AddList onSave={addList} />}
+      {loading ? (
+        <div className="spinnerContainer">
+          <div className="spinner-grow text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <div className="spinner-grow text-secondary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <div className="spinner-grow text-success" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <div className="spinner-grow text-danger" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <div className="spinner-grow text-warning" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="container">
+          {/* App Header */}
+          <Header
+            changeTextAndColor={showAddList}
+            showForm={() => setShowAddList(!showAddList)}
+          />
+          {/* Revealing the Add List Form */}
+          {showAddList && <AddList onSave={addList} />}
 
-        {/* Displaying Lists */}
-        {lists.length > 0 ? <Lists lists={lists} /> : "No List Found!"}
-      </div>
+          {/* Displaying Lists */}
+          {lists.length > 0 ? (
+            <Lists onDelete={deleteList} onEdit={editList} lists={lists} />
+          ) : (
+            "No List Found!"
+          )}
+        </div>
+      )}
     </>
   );
 }
